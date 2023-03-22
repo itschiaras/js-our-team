52ea4cc4322f7b27482c3c7db074fe9b42659284
ChiaraS97 <chiara.sucato97@gmail.com>
2023-03-22 14:17:23 +0100

    for loop + create template and add it in HTML

--- js/script.js
+++ js/script.js
@@ -13,48 +13,76 @@ BONUS 2:
 Organizzare i singoli membri in card/schede
 */
 
+// creo l'array di oggetti
+
 const team = [
     {
         name: 'Wayne',
         surname: 'Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpg'
+        image: './img/wayne-barnett-founder-ceo.jpg'
 
     },
     {
         name: 'Angela',
         surname: 'Caroll',
         role: 'Chief Editor',
-        image: 'angela-caroll-chief-editor.jpg'
+        image: './img/angela-caroll-chief-editor.jpg'
 
     },
     {
         name: 'Walter',
         surname: 'Gordon',
         role: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpg'
+        image: './img/walter-gordon-office-manager.jpg'
 
     },
     {
         name: 'Angela',
         surname: 'Lopez',
         role: 'Social Media Manager',
-        image: 'angela-lopez-social-media-manager.jpg'
+        image: './img/angela-lopez-social-media-manager.jpg'
 
     },
     {
         name: 'Scott',
         surname: 'Estrada',
         role: 'Developer',
-        image: 'scott-estrada-developer.jpg'
+        image: './img/scott-estrada-developer.jpg'
 
     },
     {
         name: 'Barbara',
         surname: 'Ramos',
         role: 'Graphic Designer',
-        image: 'barbara-ramos-graphic-designer.jpg'
+        image: './img/barbara-ramos-graphic-designer.jpg'
 
     },
 
 ]
+
+// prendo il div dentro a cui inserirò le card con le info dei membri del team
+const row = document.querySelector('.row');
+
+// creo una variabile inizialmente vuota dove andrò in seguito a salvare il mio template
+
+let rowTpl = '';
+
+// ciclo for per inserire ogni valore di ogni membro nel template
+for (let i = 0; i < team.length; i++ ) {
+    let member = team[i];
+    const tpl = `<div class="col">
+        <div class="card">
+            <img src="${member.image}" alt="" class="">
+            <div class="card-body text-center">
+                <div>${member.name} ${member.surname}</div>
+                <div>${member.role}</div>
+            </div>
+        </div>
+    </div>`
+
+    rowTpl += tpl;
+}
+
+//aggiungo il template all'html
+row.innerHTML = rowTpl;
